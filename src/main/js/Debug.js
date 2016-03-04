d9a2439c4c00d6e239668120e7da2b58dec9b9c0
ohtomi <ohtomi.kenichi@gmail.com>
2016-03-04 23:14:32 +0900

    Dump dataset when link on debug component clicked

--- src/main/js/Debug.js
+++ src/main/js/Debug.js
@@ -6,6 +6,33 @@ import {handMode} from './Constant';
 
 export default class Debug extends React.Component {
 
+    static get propTypes() {
+        return {
+            dataset: React.PropTypes.array,
+            mode: React.PropTypes.object,
+            strokeWidth: React.PropTypes.number,
+            strokeColor: React.PropTypes.string
+        };
+    }
+
+    dump(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        for (var i = 0; i < this.props.dataset.length; i++) {
+            var d = this.props.dataset[i];
+            var message = '';
+            message += 'width: ' + d.strokeWidth;
+            message += ', color: ' + d.strokeColor;
+            message += ', value: [';
+            for (var j = 0; j < d.values.length; j++) {
+                var p = d.values[j];
+                message += '(' + p[0] + ', ' + p[1] + '), ';
+            }
+            message += ']';
+            console.log(message);
+        }
+    }
+
     render() {
         const mode = this.props.mode === handMode ? 'hand' : 'line';
         const strokeWidth = this.props.strokeWidth;
@@ -16,6 +43,7 @@ export default class Debug extends React.Component {
                 <span>mode: {mode} </span>
                 <span>stroke-width: {strokeWidth} </span>
                 <span>stroke-color: {strokeColor} </span>
+                <span><a href="#" ref="dump" onClick={ev => this.dump(ev)}>Dump data</a> </span>
             </div>
         );
     }
