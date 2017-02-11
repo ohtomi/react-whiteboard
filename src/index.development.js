e57b454a5afd81728e0a0c610cb07d07f2fbcdbc
ohtomi <ohtomi.kenichi@gmail.com>
2017-02-11 22:37:07 +0900

    Add index.js

---
+++ src/index.development.js
@@ -0,0 +1,10 @@
+import Whiteboard from './Whiteboard';
+import EventCreator from './EventCreator';
+import SvgConverter from './SvgConverter';
+
+
+export {
+    Whiteboard,
+    EventCreator,
+    SvgConverter,
+};
