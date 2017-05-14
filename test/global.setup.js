107b9d368a835b79fbd767427e63f138a32f95ad
ohtomi <ohtomi.kenichi@gmail.com>
2017-05-14 21:55:27 +0900

    Remove global.setup.js

--- test/global.setup.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-
-const jsdom = require("jsdom");
-const {JSDOM} = jsdom;
-
-global.document = new JSDOM('<body></body>');
-global.window = document.window;
-global.navigator = window.navigator;
