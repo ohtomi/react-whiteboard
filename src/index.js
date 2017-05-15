7370d1c920c5001600658d32230c73c040bde88a
ohtomi <ohtomi.kenichi@gmail.com>
2017-05-15 21:53:38 +0900

    Remove EventCreator.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import Whiteboard from './Whiteboard';
-import EventCreator from './EventCreator';
 import SvgConverter from './SvgConverter';
 
 
 module.exports = {
     Whiteboard,
-    EventCreator,
     SvgConverter,
 };
