8fcc2f03cf547437d5e97dfc39b7698eb444b701
ohtomi <ohtomi.kenichi@gmail.com>
2017-05-21 14:44:34 +0900

    Remove unnecessary variable

--- src/CursorPane.js
+++ src/CursorPane.js
@@ -15,14 +15,13 @@ export default class CursorPane extends React.Component {
     }
 
     componentDidMount() {
-        const that = this;
         this.cursorLayer.addEventListener('mousemove', (ev) => {
-            const point = that.tweakPoint([ev.offsetX, ev.offsetY]);
-            that.context.events.pushPoint(point[0], point[1]);
+            const point = this.tweakPoint([ev.offsetX, ev.offsetY]);
+            this.context.events.pushPoint(point[0], point[1]);
         });
         this.cursorLayer.addEventListener('click', (ev) => {
             const point = [ev.offsetX - 2, ev.offsetY + 2];
-            that.context.events.changeMode(point);
+            this.context.events.changeMode(point);
         });
     }
 
