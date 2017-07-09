01e52374856d6e01e69a4d5a0884a1a723ffc7a6
ohtomi <ohtomi.kenichi@gmail.com>
2017-07-09 22:38:22 +0900

    Move event list reducer from canvas pane to data holder

--- src/CanvasPane.js
+++ src/CanvasPane.js
@@ -25,113 +25,33 @@ export default class CanvasPane extends React.Component {
     }
 
     drawWhiteboardCanvas() {
-        return this.props.dataHolder.eventList
-            .reduce((prev, element) => {
-                if (!element.type) {
-                    prev.forEach(p => {
-                        p.push({
-                            type: null,
-                            strokeWidth: null,
-                            strokeColor: null,
-                            values: [],
-                        });
-                    });
-                    return prev;
-                }
-
-                if (element.type === Constants.SVG_ELEMENT_TYPE.LINE) {
-                    if (!prev[element.layer]) {
-                        prev[element.layer] = [{
-                            type: null,
-                            strokeWidth: null,
-                            strokeColor: null,
-                            values: [],
-                        }];
-                    }
-
-                    let last = prev[element.layer][prev[element.layer].length - 1];
-                    if (last.type === element.type) {
-                        last.type = element.type;
-                        last.strokeWidth = element.strokeWidth;
-                        last.strokeColor = element.strokeColor;
-                        last.values.push(element.point);
+        return this.props.dataHolder.drawDataList().map((element, index) => {
+            if (element.type === Constants.SVG_ELEMENT_TYPE.LINE) {
+                const k = index;
+                const d = element.values.map((point, index) => {
+                    if (index === 0) {
+                        return 'M ' + point.x + ' ' + point.y;
                     } else {
-                        prev[element.layer].push({
-                            type: element.type,
-                            strokeWidth: element.strokeWidth,
-                            strokeColor: element.strokeColor,
-                            values: [element.point],
-                        });
+                        return 'L ' + point.x + ' ' + point.y;
                     }
-                    return prev;
+                });
 
-                } else if (element.type === Constants.SVG_ELEMENT_TYPE.IMAGE) {
-                    if (!prev[element.layer]) {
-                        prev[element.layer] = [{
-                            type: null,
-                            strokeWidth: null,
-                            strokeColor: null,
-                            values: [],
-                        }];
-                    }
+                return (
+                    <path key={k} d={d.join(' ')} fill="none" stroke={element.strokeColor} strokeWidth={element.strokeWidth}></path>
+                );
 
-                    prev[element.layer].push({
-                        type: element.type,
-                        values: [element.image],
-                    });
-                    return prev;
+            } else if (element.type === Constants.SVG_ELEMENT_TYPE.IMAGE) {
+                const k = index;
+                const image = element.values[0];
 
-                } else {
-                    return prev;
-                }
-            }, [[{
-                type: null,
-                strokeWidth: null,
-                strokeColor: null,
-                values: [],
-            }]])
-            .filter((element, index) => {
-                return this.props.dataHolder.renderLayers[index];
-            })
-            .reduce((prev, element) => {
-                return prev.concat(element);
-            }, [])
-            .filter((element) => {
-                if (element.type === Constants.SVG_ELEMENT_TYPE.LINE) {
-                    return element.values.length > 1;
-                } else if (element.type === Constants.SVG_ELEMENT_TYPE.IMAGE) {
-                    return element.values.length === 1;
-                } else {
-                    return true;
-                }
-            })
-            .map((element, index) => {
-                if (element.type === Constants.SVG_ELEMENT_TYPE.LINE) {
-                    const k = index;
-                    const d = element.values.map((point, index) => {
-                        if (index === 0) {
-                            return 'M ' + point.x + ' ' + point.y;
-                        } else {
-                            return 'L ' + point.x + ' ' + point.y;
-                        }
-                    });
+                return (
+                    <image key={k} width={image.width} height={image.height} xlinkHref={image.dataUrl}></image>
+                );
 
-                    return (
-                        <path key={k} d={d.join(' ')} fill="none" stroke={element.strokeColor} strokeWidth={element.strokeWidth}></path>
-                    );
-
-                } else if (element.type === Constants.SVG_ELEMENT_TYPE.IMAGE) {
-                    const k = index;
-                    const image = element.values[0];
-
-                    return (
-                        <image key={k} width={image.width} height={image.height} xlinkHref={image.dataUrl}></image>
-                    );
-
-                } else {
-                    return null;
-                }
-            });
+            } else {
+                return null;
+            }
+        });
     }
 }
 
--- src/DataHolder.js
+++ src/DataHolder.js
@@ -9,6 +9,89 @@ export default class DataHolder {
         this.renderLayers = [];
     }
 
+    drawDataList() {
+        return this.eventList
+            .reduce((prev, element) => {
+                if (!element.type) {
+                    prev.forEach(p => {
+                        p.push({
+                            type: null,
+                            strokeWidth: null,
+                            strokeColor: null,
+                            values: [],
+                        });
+                    });
+                    return prev;
+                }
+
+                if (element.type === Constants.SVG_ELEMENT_TYPE.LINE) {
+                    if (!prev[element.layer]) {
+                        prev[element.layer] = [{
+                            type: null,
+                            strokeWidth: null,
+                            strokeColor: null,
+                            values: [],
+                        }];
+                    }
+
+                    let last = prev[element.layer][prev[element.layer].length - 1];
+                    if (last.type === element.type) {
+                        last.type = element.type;
+                        last.strokeWidth = element.strokeWidth;
+                        last.strokeColor = element.strokeColor;
+                        last.values.push(element.point);
+                    } else {
+                        prev[element.layer].push({
+                            type: element.type,
+                            strokeWidth: element.strokeWidth,
+                            strokeColor: element.strokeColor,
+                            values: [element.point],
+                        });
+                    }
+                    return prev;
+
+                } else if (element.type === Constants.SVG_ELEMENT_TYPE.IMAGE) {
+                    if (!prev[element.layer]) {
+                        prev[element.layer] = [{
+                            type: null,
+                            strokeWidth: null,
+                            strokeColor: null,
+                            values: [],
+                        }];
+                    }
+
+                    prev[element.layer].push({
+                        type: element.type,
+                        values: [element.image],
+                    });
+                    return prev;
+
+                } else {
+                    return prev;
+                }
+            }, [[{
+                type: null,
+                strokeWidth: null,
+                strokeColor: null,
+                values: [],
+            }]])
+            .filter((element, index) => {
+                return this.renderLayers[index];
+            })
+            .reduce((prev, element) => {
+                return prev.concat(element);
+            }, [])
+            .filter((element) => {
+                if (element.type === Constants.SVG_ELEMENT_TYPE.LINE) {
+                    return element.values.length > 1;
+                } else if (element.type === Constants.SVG_ELEMENT_TYPE.IMAGE) {
+                    return element.values.length === 1;
+                } else {
+                    return true;
+                }
+            });
+    }
+
     startDrawing(strokeWidth, strokeColor, point) {
         this.eventList.push({
             type: Constants.SVG_ELEMENT_TYPE.LINE,
