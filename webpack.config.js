128a9ec82d2c95d7897223d19b4dcff40d44fcd7
ohtomi <ohtomi.kenichi@gmail.com>
2017-07-30 11:19:41 +0900

    Update library name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname + '/dist'),
         filename: 'index.js',
-        library: 'shared-components',
+        library: 'ReactWhiteboard',
         libraryTarget: 'umd'
     },
     module: {
