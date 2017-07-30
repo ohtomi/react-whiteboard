5d9261bfbc1e4b6482f7449b26b859b41260d2c9
ohtomi <ohtomi.kenichi@gmail.com>
2017-07-30 10:56:44 +0900

    Update dummy test case and run it on Travis CI

--- test/dummy.test.js
+++ test/dummy.test.js
@@ -2,9 +2,13 @@ import test from 'ava';
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import {Whiteboard} from '../dist/';
+import {Whiteboard, EventStream, EventStore} from '../dist/';
 
 test('svg exist', t => {
-    const wrapper = shallow(<Whiteboard width={800} height={600}></Whiteboard>);
+    const events = new EventStream();
+    const eventStore = new EventStore();
+    const width = 800;
+    const height = 600;
+    const wrapper = shallow(<Whiteboard events={events} eventStore={eventStore} width={width} height={height}/>);
     t.truthy(wrapper.find('svg'));
 });
