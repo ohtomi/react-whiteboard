4259dcd4efc956ac4a7083ecf2486159fa7f6024
ohtomi <ohtomi.kenichi@gmail.com>
2017-08-06 13:06:33 +0900

    Tweak flowtype annotations

--- src/Constants.js
+++ src/Constants.js
@@ -15,15 +15,17 @@ export const SVG_ELEMENT_TYPE = {
     IMAGE: 'IMAGE'
 };
 
-export type ModeType = $Either<typeof MODE.HAND,
-    typeof MODE.DRAW_LINE,
-    typeof MODE.DRAG_IMAGE,
-    typeof MODE.NW_RESIZE_IMAGE,
-    typeof MODE.NE_RESIZE_IMAGE,
-    typeof MODE.SE_RESIZE_IMAGE,
-    typeof MODE.SW_RESIZE_IMAGE>
+export type ModeType =
+    typeof MODE.HAND |
+    typeof MODE.DRAW_LINE |
+    typeof MODE.DRAG_IMAGE |
+    typeof MODE.NW_RESIZE_IMAGE |
+    typeof MODE.NE_RESIZE_IMAGE |
+    typeof MODE.SE_RESIZE_IMAGE |
+    typeof MODE.SW_RESIZE_IMAGE;
 
-export type ResizeType = $Either<typeof MODE.NW_RESIZE_IMAGE,
-    typeof MODE.NE_RESIZE_IMAGE,
-    typeof MODE.SE_RESIZE_IMAGE,
-    typeof MODE.SW_RESIZE_IMAGE>;
+export type ResizeType =
+    typeof MODE.NW_RESIZE_IMAGE |
+    typeof MODE.NE_RESIZE_IMAGE |
+    typeof MODE.SE_RESIZE_IMAGE |
+    typeof MODE.SW_RESIZE_IMAGE;
