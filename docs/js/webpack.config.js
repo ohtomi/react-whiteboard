f329cf50662fc2550cd7df0ce9dd8b7996aae575
ohtomi <ohtomi.kenichi@gmail.com>
2018-07-21 20:41:50 +0900

    Update demo.js

--- docs/js/webpack.config.js
+++ docs/js/webpack.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
     entry: './index.js',
     output: {
+        path: path.resolve(__dirname),
         filename: 'demo.js'
     },
     module: {
