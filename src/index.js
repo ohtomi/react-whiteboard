4c300c5e64a45f68fa53beba0c2229ca53c6e5ca
ohtomi <ohtomi.kenichi@gmail.com>
2019-03-26 21:29:22 +0900

    Enable experimental export * as forms in .flowconfig

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // @flow
 
-import * as Constants from './Constants'
-
 export {Whiteboard} from './Whiteboard'
 export {EventStream} from './EventStream'
 export {EventStore} from './EventStore'
 export {SvgConverter} from './SvgConverter'
-export {Constants}
+export * as Constants from './Constants'
