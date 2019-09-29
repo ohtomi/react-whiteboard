e6a4692289330f78030cdcb832195d85eace88ca
ohtomi <ohtomi.kenichi@gmail.com>
2019-09-29 20:31:00 +0900

    Reformat code

--- src/CanvasPane.tsx
+++ src/CanvasPane.tsx
@@ -1,4 +1,4 @@
-import * as  React from 'react'
+import * as React from 'react'
 
 import {AnyReducedEventType, EventStore, ImageDataType, isReducedImageEvent, isReducedLineEvent, PointDataType} from './EventStore'
 
