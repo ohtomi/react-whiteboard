5e3bb4382b1ca71ba06bbe1f94492d99cb09ff7d
ohtomi <ohtomi.kenichi@gmail.com>
2019-10-08 21:18:25 +0900

    Export enum and interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import {ModeEnum, SvgElementEnum} from './Constants'
 
 export {Whiteboard} from './Whiteboard'
-export {ChangeStrokeColor, ChangeStrokeWidth, EventStream, EventListener} from './EventStream'
-export {EventStore, ImageData, ImageEvent, MouseMoveData, PointData, PointEvent, StopEvent} from './EventStore'
+export {ChangeStrokeColor, ChangeStrokeWidth, EventListener, EventNameEnum, EventStream} from './EventStream'
+export {
+    EventStore,
+    EventStoreProtocol,
+    ImageData,
+    ImageEvent,
+    isPointEvent,
+    isImageEvent,
+    isReducedImageEvent,
+    isReducedLineEvent,
+    MouseMoveData,
+    PointData,
+    PointEvent,
+    StopEvent
+} from './EventStore'
 export {SvgConverter} from './SvgConverter'
 export {ModeEnum, SvgElementEnum}
