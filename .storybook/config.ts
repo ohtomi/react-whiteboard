c638edc7b5e4131cd91cfa17d5f41929b868b089
Roman Baiocco <roman@karat.com>
2020-03-20 15:51:14 -0700

    Publish Build, remove trash

--- .storybook/config.ts
+++
@@ -1,9 +0,0 @@
-import {configure} from '@storybook/react'
-
-const req = require.context('../stories', true, /\.tsx$/)
-
-function loadStories() {
-    req.keys().forEach(req)
-}
-
-configure(loadStories, module)
